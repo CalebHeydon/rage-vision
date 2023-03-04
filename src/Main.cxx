5fbcaa563689c6c097cff02696518f84b548bc49
Caleb <caleb.heydon@gmail.com>
2023-03-04 11:59:52 -0500

    Updated copyright year

--- src/Main.cxx
+++ src/Main.cxx
@@ -17,7 +17,7 @@ All rights reserved.
 int main(int argc, char **argv)
 {
     std::cout << "RageVision " << Constants::kVersion << "\n";
-    std::cout << "Copyright (c) 2022, Caleb Heydon\n";
+    std::cout << "Copyright (c) 2023, Caleb Heydon\n";
     std::cout << "All rights reserved.\n\n";
 
     if (argc < 1)
